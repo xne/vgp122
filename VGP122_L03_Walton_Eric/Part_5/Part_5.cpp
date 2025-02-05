11fe9f66e4ef4655e2bf35d725aa5789659fc11e
Erika <2530460+xne@users.noreply.github.com>
2025-02-05 16:17:55 -0500

    Bugfix

--- VGP122_L03_Walton_Eric/Part_5/Part_5.cpp
+++ VGP122_L03_Walton_Eric/Part_5/Part_5.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 
 int toCelsius(int);
@@ -21,13 +22,12 @@ int main()
 	return 0;
 }
 
-// TODO: Could use std::round instead of truncating to int
 int toFahrenheit(int c)
 {
-	return c * (9.0f / 5) + 32;
+	return std::round(c * (9.0f / 5) + 32);
 }
 
 int toCelsius(int f)
 {
-	return (f - 32) * (5.0f / 9);
+	return std::round((f - 32) * (5.0f / 9));
 }
